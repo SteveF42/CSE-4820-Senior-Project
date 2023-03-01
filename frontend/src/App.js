ccaa057bfa46294755d9d07f8c0301e6d7ede599
Steve Flores <stevewflores43@gmail.com>
2023-02-28 20:26:35 -0800

    access dysh API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://fresh-dysh.com/api/v1/docs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Access dysh API
         </a>
       </header>
     </div>
